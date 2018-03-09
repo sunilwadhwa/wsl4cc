a0cff9ef997bc3fb8d0ee3ccb386596b67078596
Cassio M. M. Pereira <cassiomartini@gmail.com>
2018-03-09 15:04:50 -0300

    handling input structures

--- src/main/java/com/sap/ateam/wsl4cc/util/ConversionUtil.java
+++ src/main/java/com/sap/ateam/wsl4cc/util/ConversionUtil.java
@@ -3,30 +3,20 @@ package com.sap.ateam.wsl4cc.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import com.sap.conn.jco.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sap.ateam.wsl4cc.Wsl4ccException;
-import com.sap.conn.jco.ConversionException;
-import com.sap.conn.jco.JCoField;
-import com.sap.conn.jco.JCoMetaData;
-import com.sap.conn.jco.JCoParameterField;
-import com.sap.conn.jco.JCoParameterFieldIterator;
-import com.sap.conn.jco.JCoParameterList;
-import com.sap.conn.jco.JCoRecord;
-import com.sap.conn.jco.JCoTable;
 
 public class ConversionUtil {
 
 	public static Object convertJCoFieldToPrimitive(JCoField field) {
 		Object ret = null;
 		int type = field.getType();
-		
+
 		switch (type) {
 			case JCoMetaData.TYPE_CHAR:
 			case JCoMetaData.TYPE_INT1:
@@ -41,7 +31,7 @@ public class ConversionUtil {
 			case JCoMetaData.TYPE_DECF34:
 				ret = field.getValue();
 				break;
-				
+
 			case JCoMetaData.TYPE_BYTE:
 			case JCoMetaData.TYPE_XSTRING:
 			case JCoMetaData.TYPE_DATE:
@@ -52,25 +42,25 @@ public class ConversionUtil {
 			case JCoMetaData.TYPE_STRUCTURE:
 				ret = field.getStructure();
 				break;
-				
+
 			case JCoMetaData.TYPE_TABLE:
 				ret = field.getTable();
 				break;
-				
+
 			default:
 				ret = null;
 				logger.error("Unrecognized type " + field.getClassNameOfValue() + " for field " + field.getName());
 				break;
 		}
-		
+
 		return ret;
 	}
-	
+
 	/**
 	 * Convert a user provided List to a JCoTable input table.
-	 * 
+	 *
 	 * @param tableName Name of input table parameter
-	 * @param table JCoTable 
+	 * @param table JCoTable
 	 * @param list User provided list
 	 * @return Resulting table
 	 * @throws Wsl4ccException
@@ -78,33 +68,33 @@ public class ConversionUtil {
 	public static JCoTable convertToJCoTable(String tableName, JCoTable table, List<?> list) throws Wsl4ccException {
 		if (list == null || table == null)
 			return table;
-		
+
 		JCoMetaData meta = table.getMetaData();
 		for (Object o : list) {
 			if (!(o instanceof Map<?,?>))
 				throw new Wsl4ccException("User input for table " + tableName + " is not a valid map.");
-			
+
 			Map<?,?> map = (Map<?,?>) o;
 			table.appendRow();
 			for (Map.Entry<?,?> e : map.entrySet()) {
 				String fieldName = (String) e.getKey();
 				Object fieldValue = (Object) e.getValue();
-				
+
 				if (meta.hasField(fieldName))
 					convertPrimitiveToJCoField(table, fieldName, fieldValue);
 				else
 					throw new Wsl4ccException("Unrecognized or invalid field " + fieldName + " in table " + tableName);
 			}
 		}
-		
+
 		return table;
 	}
-	
+
 	public static void convertPrimitiveToJCoField(JCoRecord record, String name, Object value) throws Wsl4ccException {
 		JCoField field = record.getField(name);
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd"); // TODO: Make dateFormat a query parameter.
 		df.setLenient(false);
-		
+
 		switch (field.getType()) {
 			case JCoMetaData.TYPE_DATE:
 				try {
@@ -115,7 +105,19 @@ public class ConversionUtil {
 					throw new Wsl4ccException(m);
 				}
 				break;
+			case JCoMetaData.TYPE_STRUCTURE:
+				JCoStructure structure = field.getStructure();
+				JCoRecordFieldIterator it = structure.getRecordFieldIterator();
+				AbstractMap map = (AbstractMap) value;
 
+				while (it.hasNextField()) {
+					JCoRecordField structField = it.nextRecordField();
+					String fname = structField.getName();
+					if (map.containsKey(fname))
+						structure.setValue(fname, map.get(fname));
+				}
+				record.setValue(name, structure);
+				break;
 			default:
 				try {
 					record.setValue(name, value);
@@ -130,19 +132,19 @@ public class ConversionUtil {
 
 	public static Map<String,Object> convertParameterListToMap(JCoParameterList pList) {
 		if (pList == null) return null;
-		
+
         Map <String,Object> pMap = new HashMap<>();
         JCoParameterFieldIterator iterator = pList.getParameterFieldIterator();
-        
+
         while (iterator.hasNextField()) {
         	JCoParameterField field = iterator.nextParameterField();
 			Object value = convertJCoFieldToPrimitive(field);
         	pMap.put(field.getName(), value);
         }
-        
+
         return pMap;
 	}
-	
+
     private static Logger logger = LoggerFactory.getLogger(ConversionUtil.class);
 
 }
